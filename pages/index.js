5b5bea2b12b2f30e722904fcab5f7bfbeadf3776
Bayo1996 <sebasv96@gmail.com>
2023-09-10 16:39:45 -0500

    Adding AI

---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [modelInput, setmodelInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ model: modelInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setmodelInput("");
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Motorcycle Models</title>
+        <link rel="icon" href="/Motorcycle.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/Motorcycle.png" className={styles.icon} />
+        <h3>Motorcycle Model Information</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="Model"
+            placeholder="Enter your motorcycle model"
+            value={modelInput}
+            onChange={(e) => setmodelInput(e.target.value)}
+          />
+          <input type="submit" value="Get info" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
